89198ce1262fe5e8fd71067c69e36f89debad9f9
devo <devollove9@gmail.com>
2024-08-26 05:07:36 -0400

    update - Update Home Page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+}
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
+  themeConfig: './theme.config.jsx'
 })
-module.exports = withNextra()
+
+module.exports = withNextra(nextConfig)
