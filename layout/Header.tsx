20af26b15fc1f8d4a8e5fcc31e3bd6557ae3fa63
devo <devollove9@gmail.com>
2024-08-26 06:16:52 -0400

    update - Update Home Page

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -1,4 +1,4 @@
-import NavigationBar from '@layout/NavigationBar';
+//import NavigationBar from '@layout/NavigationBar';
 const Header = () => (
   <>
     <header
@@ -15,7 +15,7 @@ const Header = () => (
           <div className="text-xs tracking-widest">Devo is doing something</div>
         </a>
         <div className="flex-grow pb-5 pr-5">
-          <NavigationBar />
+          "NavigationBar 1"
         </div>
       </div>
     </header>
