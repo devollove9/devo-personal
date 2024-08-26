a8449f655a1f22c5209b539ea93479187302d2e3
devo <devollove9@gmail.com>
2024-08-26 05:33:56 -0400

    update - Update Home Page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  experimental: {
+    appDir: true,
+  },
 }
 
 const withNextra = require('nextra')({
